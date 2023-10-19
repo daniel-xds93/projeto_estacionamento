99c8da40a7f8f504b00ce1e14581e2734c79e476
Daniel Xavier <daniel.xds@hotmail.com>
2023-10-19 18:31:42 -0300

    foi modificado o arquivo novo.html responsavel por cadastrar novos filmes, e para que seja listado foi criada uma rota para pegar os dados e aplicar na lista

--- estacionamento.py
+++ estacionamento.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 # a linha abaixo eu crio uma classe
 class Veiculo:
@@ -10,6 +10,12 @@ class Veiculo:
         self.cor = cor
         self.proprietario = proprietario
 
+carro01 = Veiculo('ABC-1234', 'Gol', 'Preto', 'Daniel')
+carro02 = Veiculo('DEF-5678', 'Civic', 'Prata', 'Denis')
+carro03 = Veiculo('GHI-9012', 'Golf', "Azul", "José")
+
+# a linha abaixo adiciona uma lista com os veiculos
+listaVeiculos = [carro01, carro02, carro03]
 
 
 # a linha abaixo eu crio a variavel responsavel por rodar meu projeto
@@ -25,16 +31,40 @@ def primeira():
 def lista():
     # listaVeiculos = ['Onix', 'Jetta', 'Civic', 'Gol']
 
-    carro01 = Veiculo('ABC-1234', 'Gol', 'Preto', 'Daniel')
-    carro02 = Veiculo('DEF-5678', 'Civic', 'Prata', 'Denis')
-    carro03 = Veiculo('GHI-9012', 'Golf', "Azul", "José")
-
-    # a linha abaixo adiciona uma lista com os veiculos
-    listaVeiculos = [carro01, carro02, carro03]
+    
 
 
     return render_template('lista.html', tituloPagina = 'Lista de Veículos',
                             carros = listaVeiculos)
 
+
+# a linha abaixo é a rota para abrir uma a tela de cadastro
+@app.route('/novo')
+def cadastra_novo(): # este método apenas chama o arquivo novo.html
+    return render_template('novo.html')
+
+# a rota abaixo é responsavel por cadastrar os dados
+@app.route('/criar', methods=['POST',])
+def cadastra_informacao():
+    # para a proxima etapa é necessario importar o "request"
+    placa = request.form['placa']
+    modelo = request.form['modelo']
+    cor = request.form['cor']
+    proprietario = request.form['proprietario']
+
+    # a linha abaixo cria uma variavel usando a classe criada
+    carroAdd = Veiculo(placa, modelo, cor, proprietario)
+
+    # a linha abaixo adiciona o veículo digitado pelo usuario 
+    # na lista de veiculos
+    listaVeiculos.append(carroAdd)
+
+    # a linha abaixo direciona para a tela de lista.html
+    #return render_template('lista.html', 
+    #                       tituloPagina = 'Lista de Veículos',
+    #                        carros = listaVeiculos)
+
+    # a linha abaixo necessita adicionar o redirect
+    return redirect('/lista')
 
 app.run()
